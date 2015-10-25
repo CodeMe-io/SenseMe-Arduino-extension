e22d663ed1e4027d52c32913171b8d0e44fb95c1
venusshum <v.shum@ucl.ac.uk>
2015-10-25 14:47:28 +0000

    Edit and tidy SenseMe Example folder structure

--- examples/200.SenseMe/Neopixel/Neopixel.ino
+++ examples/200.SenseMe/Neopixel/Neopixel.ino
@@ -5,7 +5,20 @@ void setup() {
   SenseMeNeopixel.begin();
 }
 
+int brightness = 0;
+int dir = 0;
+
 void loop() {
   // put your main code here, to run repeatedly:
-  SenseMeNeopixel.setColor(255,255,255, 15);
+  SenseMeNeopixel.setColor(255,255,255, brightness);
+  if (dir == 0) 
+	brightness ++;
+  else 
+	brightness --;
+  if (brightness == 255)
+    dir = 1;
+  if (brightness == 0)
+    dir = 0;
+  delay(10);
+  
 }
--- examples/200.SenseMe/SimplePet/SimplePet.ino
+++ examples/200.SenseMe/SimplePet/SimplePet.ino
@@ -3,65 +3,141 @@
 #include <SenseMeAccelerometer.h>
 #include <SenseMeNeopixel.h>
 #include <SenseMeLightSensor.h>
-
-float accel[3];
+#include <SenseMeButton.h>
+int i = 0;
 
 void setup() {
   // put your setup code here, to run once:
   SenseMeLEDMatrix.begin(); //initialise matrix
   SenseMeAccelerometer.begin(); //initialise accelerometer
+  SenseMeButton.begin();
+  boolean stage1 = true;
+  while(stage1)
+  {
+    long randNum = random(100);
+    if(randNum < 80)
+    {
+      SenseMeLEDMatrix.setFace("happy");
+      delay(1000);
+    }
+    else
+    {
+      SenseMeLEDMatrix.setFace("sad");
+      boolean sad = true;
+      while(sad)
+      {
+        if(getMag()>1.5)
+        {
+          SenseMeLEDMatrix.setFace("laugh");	
+          delay( 1000 );
+          sad = false;
+          i++;
+        }
+      }
+    }
+    if(i>5)
+    {
+     stage1 = false; 
+     i = 0;
+    }
+  }
+  
+  boolean stage2 = true;
+  while(stage2)
+  {
+    long randNum = random(100);
+    if(randNum < 80)
+    {
+      SenseMeLEDMatrix.setFace("happy");
+      delay(1000);
+    }
+    else
+    {
+      SenseMeLEDMatrix.setFace("yawn");
+      boolean upright = true;
+      while(upright)
+      {
+        if (SenseMeAccelerometer.getY()  < ( -0.9 ) )
+        {
+          upright = false;
+        }
+      }
+      SenseMeLEDMatrix.setFace("sleep");		
+      delay( 1000 );
+      boolean asleep = true;
+      while (asleep)
+      {
+        if (SenseMeAccelerometer.getX() < ( -0.9 ) )
+        {
+          asleep = false;
+        }
+      }
+      i++;
+    }
+    if(i>5)
+    {
+     stage2 = false; 
+    }
+  }
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  long randNum = random(100); // generates a random number between 1 and 100 to decide how your pet is feeling
-  if (randNum < 80)
+  long randNum = 	random( 0 , 100 ) ;
+  if (randNum < 80 )
   {
-    SenseMeLEDMatrix.setFace("happy"); //if the number is below 80 your pet will be happy :)
-    delay(1000); //wait for 1 second
+    SenseMeLEDMatrix.setFace("happy");		
+    delay( 1000 );
   }
-  else if(randNum < 90)
+  else
   {
-    SenseMeLEDMatrix.setFace("sad"); //if the number is between 80 and 90 your pet will become sad :(
-    boolean sad = true; // this makes a variable called sad which can either be true or false (using the word boolean means that it can be true or false)
-    while(sad) //this loops for as long as your pet is sad
+    if (randNum  < 90 )
     {
-      delay(50); // this waits for one twentieth of a second in preparation for taking a measurement from the accelerometer
-      float absAccel = SenseMeAccelerometer.magnitude(); // this return the amount that the cube is accelerating. 1 is the acceleration due to gravity more than 1 means that you are shaking it
-      if(absAccel>1.5)// this tests whether the cube is moving enough
+      SenseMeLEDMatrix.setFace("sad");	
+      boolean sad = true;
+      while ( sad )
       {
-        SenseMeLEDMatrix.setFace("laugh"); // if the cube is moving then your pet will laugh
-        delay(1000); // wait for 1 second
-        sad = false; // your pet is no longer sad 
+        float Acceleration = getMag();
+        if (Acceleration > ( 1.5 ) )
+        {
+          SenseMeLEDMatrix.setFace("laugh");	
+          delay( 1000 );
+          sad = false;
+        }
       }
-      // if there is no movement then you pet continues to be sad  
+    }
+    else
+    {
+      SenseMeLEDMatrix.setFace("yawn");	
+      boolean upright = true;
+      while (upright)
+      {
+        if (SenseMeAccelerometer.getY() < ( -0.9 ) )
+        {
+          upright = false;
+        }
+      }
+
+      SenseMeLEDMatrix.setFace("sleep");	
+      delay( 1000 );
+      boolean asleep = true;
+      while (asleep == true )
+      {
+        if (SenseMeAccelerometer.getX() < ( -0.9 ) )
+        {
+          asleep = false;
+        }
+      }
+
     }
   }
-  else 
-  {
-    SenseMeLEDMatrix.setFace("yawn"); // if the number is between 90 and 100 then you pet is tired :O
-    boolean upright = true;
-    while(upright) // this loops while the cube is sitting upright making the code have to wait until the cube is laid down
-    {
-      SenseMeAccelerometer.xyz(accel); // this gathers the co-ordinates from the accelerometer
-      if(accel[0]>-0.1) // this tests whether the cube has been laid down
-      {
-        upright = false;
-      } 
-    }
-    SenseMeLEDMatrix.setFace("sleep");// if your pet is laid on his back then he will go to sleep
-    delay(1000);
-    boolean asleep = true;
-    while(asleep)// this will loop for as long as your pet is asleep (laid on his back)
-    {
-      SenseMeAccelerometer.xyz(accel);
-      if(accel[0]<-0.9)
-      {
-        asleep = false; // if you pet is put upright then he wakes up
-      }  
-    }
-  }
-  //now we go back to the start of the loop
+  
+
 }
 
-
+float getMag()
+{
+  float magnitude = sqrt(pow(SenseMeAccelerometer.getX(), 2)+ pow(SenseMeAccelerometer.getX(), 2)+pow(SenseMeAccelerometer.getX(), 2));
+  return magnitude;
+}
