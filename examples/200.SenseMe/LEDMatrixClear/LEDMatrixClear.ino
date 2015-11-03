4b2235f20fbb449a1c2c176e4a8a072105eee1ba
venusshum <v.shum@ucl.ac.uk>
2015-11-03 21:27:26 +0000

    add LED Clear Matrix example

---
+++ examples/200.SenseMe/LEDMatrixClear/LEDMatrixClear.ino
@@ -0,0 +1,13 @@
+#include <SenseMeLEDMatrix.h>
+#include <Wire.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  SenseMeLEDMatrix.begin();
+  SenseMeLEDMatrix.clear();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
